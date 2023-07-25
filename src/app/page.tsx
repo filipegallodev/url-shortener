475caaf836d24510f1ae014fbae5d667df0cc33c
Filipe <filipegallodev@hotmail.com>
2023-07-25 20:28:28 -0300

    header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Header from "@/components/Header/Header";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
-      <h1>URL Shortner</h1>
-    </main>
+    <>
+      <Header />
+      <main>
+        <section>
+          <h2>Basta colocar o link que desejar encurtar logo abaixo!</h2>
+        </section>
+      </main>
+    </>
   );
 }
